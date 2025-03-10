752f813125a4c23a0016b200adf21e92cc74f559
Nanae-j <nanae9577@gmail.com>
2025-03-11 08:52:00 +0900

    [update]フォームのフロント構築

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { FiSend } from "react-icons/fi";
 
 const PostForm = () => {
   const [colors, setColors] = useState([
@@ -13,15 +14,14 @@ const PostForm = () => {
     { label: "モヤモヤ", value: "#8C00FF", checked: false },
   ]);
 
-  const [color, setColor] = useState("");
+  // const [color, setColor] = useState("");
 
-  const handleChange = (e) => {
+  const handleChange = (e: { target: { value: string } }) => {
     const newColors = colors.map((color) => {
       const newColor = { ...color };
 
       if (newColor.label === e.target.value) {
         newColor.checked = !newColor.checked;
-        setColor(newColor.label);
       }
 
       return newColor;
@@ -31,15 +31,34 @@ const PostForm = () => {
   };
 
   return (
-    <div className="mb-15">
+    <div className="mb-28">
       <form action="">
+        <div className="mb-3 text-right">
+          <button className="bg-default hover:outline-default hover:text-default rounded-md px-6 py-2 text-xl text-white transition-colors hover:bg-white hover:outline-2">
+            <FiSend />
+          </button>
+        </div>
         <textarea
           name=""
           id=""
           placeholder="Share your thoughts and feelings..."
-          className="border-whitesmoke h-32 w-full resize-none rounded-2xl border-1 bg-white p-3"
+          className="border-whitesmoke outline-default h-32 w-full resize-none rounded-2xl border-1 bg-white p-3"
         />
-        <p>今の気分は：{color}</p>
+        <p className="flex items-center gap-1">
+          今の気分は：
+          {colors.map((color) => {
+            return color.checked ? (
+              <span
+                key={color.value}
+                className={`${color.value === "#8C00FF" ? "text-[#8C00FF]" : ""} ${color.value === "#FF0004" ? "text-[#FF0004]" : ""} ${color.value === "#FF8800" ? "text-[#FF8800]" : ""} ${color.value === "#FFE600" ? "text-[#FFE600]" : ""} ${color.value === "#73FF00" ? "text-[#73FF00]" : ""} ${color.value === "#FB00FF" ? "text-[#FB00FF]" : ""} ${color.value === "#0071FF" ? "text-[#0071FF]" : ""}`}
+              >
+                {color.label}
+              </span>
+            ) : (
+              ""
+            );
+          })}
+        </p>
         <div className="my-5 flex items-center gap-5">
           {colors.map((color, index) => {
             return (
@@ -58,16 +77,7 @@ const PostForm = () => {
                     className="flex cursor-pointer flex-col items-center"
                   >
                     <div
-                      className={`
-                        ${color.value === '#8C00FF' ? 'bg-[#8C00FF]' : ''}
-                        ${color.value === '#FF0004' ? 'bg-[#FF0004]' : ''}
-                        ${color.value === '#FF8800' ? 'bg-[#FF8800]' : ''}
-                        ${color.value === '#FFE600' ? 'bg-[#FFE600]' : ''}
-                        ${color.value === '#73FF00' ? 'bg-[#73FF00]' : ''}
-                        ${color.value === '#FB00FF' ? 'bg-[#FB00FF]' : ''}
-                        ${color.value === '#0071FF' ? 'bg-[#0071FF]' : ''}
-                        ${color.checked === true ? 'h-15 w-15' : ''}
-                        h-12 w-12 rounded-full transition-all`}
+                      className={` ${color.value === "#8C00FF" ? "bg-[#8C00FF]" : ""} ${color.value === "#FF0004" ? "bg-[#FF0004]" : ""} ${color.value === "#FF8800" ? "bg-[#FF8800]" : ""} ${color.value === "#FFE600" ? "bg-[#FFE600]" : ""} ${color.value === "#73FF00" ? "bg-[#73FF00]" : ""} ${color.value === "#FB00FF" ? "bg-[#FB00FF]" : ""} ${color.value === "#0071FF" ? "bg-[#0071FF]" : ""} ${color.checked === true ? "h-12 w-12" : ""} h-8 w-8 rounded-full transition-all`}
                     ></div>
                   </label>
                 </div>
@@ -75,12 +85,6 @@ const PostForm = () => {
             );
           })}
         </div>
-        <button className="flex items-center gap-3 rounded-md px-1 py-3 transition-all ease-in">
-          <span className="font-black tracking-wider text-sky-400">
-            COLOR IT ...
-          </span>
-          <span className="h-[10px] w-[10px] animate-ping rounded-full bg-sky-400 opacity-75"></span>
-        </button>
       </form>
     </div>
   );
