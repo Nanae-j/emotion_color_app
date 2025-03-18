bb120fb39fa84b34b6e967bf433ae1a8b3f30424
Nanae-j <nanae9577@gmail.com>
2025-03-18 19:30:42 +0900

    [update]フォローの楽観的UI更新実装

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { followAction } from "@/actions/followAction";
+import { useOptimistic } from "react";
 
 interface FollowButtonProps {
   isViewingOwnProfile: boolean;
@@ -13,18 +14,53 @@ const FollowButton = ({
   isFollowing,
   currentUserId,
 }: FollowButtonProps) => {
-  const followActionWithFlag = followAction.bind(
-    null,
-    isFollowing,
-    currentUserId
-  );
+  // const followActionWithFlag = followAction.bind(
+  //   null,
+  //   isFollowing,
+  //   currentUserId
+  // );
+
+  const [optimisticFollow, changeOptimisticFollow] = useOptimistic<
+    { isFollowing: boolean },
+    void
+  >({ isFollowing }, (currentState) => ({
+    isFollowing: !currentState.isFollowing,
+  }));
+
+  const handleFollowAction = async () => {
+    try {
+      changeOptimisticFollow();
+      await followAction(isFollowing, currentUserId);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getButtonContent = () => {
+    if (optimisticFollow.isFollowing) {
+      return "フォローを解除する";
+    }
+
+    return "フォローする";
+  };
+
+  const getButtonStyle = () => {
+    if (isViewingOwnProfile) {
+      return "bg-default pointer-events-none opacity-20";
+    } else if (!isViewingOwnProfile && optimisticFollow.isFollowing) {
+      return "bg-white text-blue-400 outline-2 outline-blue-400 hover:bg-blue-400 hover:text-white";
+    } else if (!isViewingOwnProfile && !optimisticFollow.isFollowing) {
+      return "text-white bg-blue-400 hover:bg-white hover:text-blue-400 hover:outline-2 hover:outline-blue-400";
+    }
+  };
+
   return (
     <div>
-      <form action={followActionWithFlag}>
+      <form action={handleFollowAction}>
         <button
-          className={`transition-color rounded-md px-10 py-3 text-white hover:bg-white hover:text-blue-400 hover:outline-2 hover:outline-blue-400 ${isViewingOwnProfile ? "bg-default pointer-events-none opacity-20" : "bg-blue-400"}`}
+          className={`transition-color rounded-md px-10 py-3 ${getButtonStyle()}`}
         >
-          {isFollowing ? "フォローを解除する" : "フォローする"}
+          {getButtonContent()}
         </button>
       </form>
     </div>
