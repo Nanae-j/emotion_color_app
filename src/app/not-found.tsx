62630edfd3f6c4063e657d6d94820ffb1b04b0a2
Nanae-j <nanae9577@gmail.com>
2025-03-19 14:08:33 +0900

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
-  return <div>このページは見つかりません</div>;
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <p className="mb-5 text-xl tracking-wider">
+          このページは見つかりません
+        </p>
+        <Link
+          href={"/"}
+          className="hover:text-red p-5 font-bold tracking-widest transition-colors"
+        >
+          TOPに戻る
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
